b23d0186034a91f0ac78b3aa5f4059d97ea9c182
Asma Awad <awadasma99@gmail.com>
2020-12-01 22:30:15 -0500

    implemented ability to look up recipes in general

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/"
 
 headers = {
-    'x-rapidapi-key': "RAPID-API-KEY",
+    'x-rapidapi-key': "8d518c5534mshba4a11ab4c1cb0ep1c69d1jsn4c0415dbddd7",
     'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
 }
 
-findRecipes = "recipes/findByIngredients"
+findRecipes = "recipes/search"
 
 @app.route('/')
 def searchRecipes():
@@ -19,9 +19,10 @@ def searchRecipes():
 @app.route('/recipes') 
 def recipe_results():
     if (str(request.args['ingredients']).strip() != ""):
-        query = {"ingredients":request.args['ingredients'],"number":"5","ranking":"1","ignorePantry":"false"}
+        query = {"query":request.args['ingredients'],"number":"10","offset":"0","instructionsRequired":"true"}
         response = requests.request("GET", url + findRecipes, headers=headers, params=query).json()
-        return render_template('recipes.html', recipes=response)
+        results = response['results']        
+        return render_template('recipes.html', recipes=results)
 
 @app.route('/recipe')
 def recipe():

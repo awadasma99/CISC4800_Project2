f6a4553507864efec31e0d28b14a230fa57b8ea4
Asma Awad <awadasma99@gmail.com>
2020-12-02 19:59:49 -0500

    commented out

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/"
 
 headers = {
-    'x-rapidapi-key': "API-KEY",
+    'x-rapidapi-key': "8d518c5534mshba4a11ab4c1cb0ep1c69d1jsn4c0415dbddd7",
     'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
 }
 
@@ -16,9 +16,9 @@ findRecipes = "recipes/search"
 def homepage():
     return render_template('index.html')
 
-@app.route('/search')
-def searchRecipes():
-    return render_template('search_page.html')
+# @app.route('/search')
+# def searchRecipes():
+#    return render_template('search_page.html')
 
 @app.route('/recipes') 
 def recipe_results():

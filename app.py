700134127bb537cb59711355dad9c1129672da12
Thrwat Najjar <thrwat101@gmail.com>
2020-12-03 20:14:23 -0500

    database and login added

--- app.py
+++ app.py
@@ -1,20 +1,38 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+from flask_sqlalchemy import SQLAlchemy
 
 import requests
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///users.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/"
 
 headers = {
-    'x-rapidapi-key': "API-KEY",
+    'x-rapidapi-key': "8d518c5534mshba4a11ab4c1cb0ep1c69d1jsn4c0415dbddd7",
     'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
 }
 
 findRecipes = "recipes/search"
 
+db = SQLAlchemy(app)
+class users(db.Model):
+    email = db.Column("email", db.String(100), primary_key= True)
+    password = db.Column("password", db.String(100))
+
+    def __int__(self, email, password):
+        self.email = email
+        self.password = password
+
 @app.route('/')
 def homepage():
     return render_template('index.html')
+   
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        return render_template("user.html")
+    else:
+        return render_template("login.html")
 
 # @app.route('/search')
 # def searchRecipes():
@@ -36,4 +54,5 @@ def recipe():
     return render_template('recipe.html', recipe=recipe_details)
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)

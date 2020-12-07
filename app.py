79e70826d894733d9523552d353ab169f0266f03
Thrwat Najjar <thrwat101@gmail.com>
2020-12-07 00:05:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,84 @@
-from flask import Flask, render_template, request, session, flash
+from flask import Flask, render_template, request, session, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from pymongo import MongoClient
+from flask_pymongo import PyMongo
+from werkzeug.urls import url_parse
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, LoginManager, current_user, logout_user,login_required, login_user
 import requests
-import itertools 
+import itertools
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+mongo = PyMongo(app)
 url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/"
 headers = {
-    'x-rapidapi-key': "",
+    'x-rapidapi-key': "8d518c5534mshba4a11ab4c1cb0ep1c69d1jsn4c0415dbddd7",
     'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
 }
 
+log = LoginManager(app)
+log.login_view = "login"
 client = MongoClient("localhost", 27017)
 db = client['mydb']
-print(db)
 coll = db["users"]
 
+class User:
+    def __init__(self, name, email, password):
+        self.email = email
+        self.name = name
+        self.password = password
 
-@app.route("/Login", methods=["POST", "GET"])
+    @staticmethod
+    def is_authenticated():
+        return True
+
+    @staticmethod
+    def is_active():
+        return True
+
+    @staticmethod
+    def check_password(password_hash, password):
+        return check_password_hash(password_hash, password)
+
+    @log.user_loader
+    def load_user(self):
+        usr = mongo.db.Users.find_one({"name": self.name})
+        if not usr:
+            return None
+        return User(name=usr["name"], email=usr["email"], password=usr["password"])
+
+class Login(FlaskForm):
+    email = StringField('email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    login = SubmitField('Login')
+
+class Signup(FlaskForm):
+    name = StringField('email', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Signup')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == "POST":
-       return render_template("user.html")
-    else:
-       return render_template("login.html")
-   
-@app.route("/Signup", methods=["POST", "GET"])
-def signup():
-    if request.method == "POST":
-        name = ["name"]
-        email = ["email"]
-        password = ["password"]
-        info = [{"name" : name, "email" : email, "password" : password}]
-        user = coll.insert_one(info)
+    form = Login()
+    if current_user.is_authenticated:
         return render_template("user.html")
+    if form.validate_on_submit():
+         user = mongo.db.Users.find_one({"email": form.email.data})
+         if user and User.check_password(user['Password'], form.password.data):
+            login_user(user)
+            return render_template("user.html")
     else:
-        return render_template("signup.html")
+        flash("Invalid username or password")
+        return render_template("login.html")
 
-@app.route("/logout")
+@app.route('/logout')
 def logout():
+    logout_user()
     return render_template("index.html")
 
 @app.route("/user")

73f05ab4f07bd8ae74fe02dd3bdf00ca7d52fdd6
Thrwat Najjar <thrwat101@gmail.com>
2020-12-18 18:09:54 -0500

    save recipe button
    not functional  yet

--- project/forms.py
+++ project/forms.py
@@ -1,5 +1,6 @@
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Length
+from wtforms.widgets import html_params, HTMLString
 from flask_wtf import FlaskForm
 from . import db
 
@@ -11,4 +12,28 @@ class EditProfileForm(FlaskForm):
     new_password = StringField('new password', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+class ButtonWidget(object):
+    """
+    Renders a multi-line text area.
+    `rows` and `cols` ought to be passed as keyword args when rendering.
+    """
+    input_type = 'submit'
 
+    html_params = staticmethod(html_params)
+
+    def __call__(self, field, **kwargs):
+        kwargs.setdefault('id', field.id)
+        kwargs.setdefault('type', self.input_type)
+        if 'value' not in kwargs:
+            kwargs['value'] = field._value()
+
+        return HTMLString('<button {params}>{label}</button>'.format(
+            params=self.html_params(name=field.name, **kwargs),
+            label=field.label.text)
+        )
+
+class ButtonField(StringField):
+    widget = ButtonWidget()
+
+class SaveRecipe(FlaskForm):
+    save = ButtonField('Save Recipe')
